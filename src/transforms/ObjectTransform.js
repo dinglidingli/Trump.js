3b9435aa41856fc701404ce5958e9c30a4466b49
Christopher Dingli <christopher@dingli.com.au>
2017-06-17 18:31:28 +1000

    Iterate over object keys because all promises should be broken equally

--- src/transforms/ObjectTransform.js
+++ src/transforms/ObjectTransform.js
@@ -3,6 +3,14 @@ import InputHandler from './../InputHandler';
 function ObjectTransform(input) {
     if(Array.isArray(input)) {
         return input.map((item) => InputHandler(item));
+    } else if(typeof input !== 'function') {
+        const newObj = {};
+
+        for (var key in input) {
+            newObj[key] = InputHandler(input[key]);
+        }
+
+        return newObj;
     }
 
     return input;
